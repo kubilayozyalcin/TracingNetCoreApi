c4b12dd106bcd0089c43781af2487c2e8e367883
Kubilay Özyalçın <kozyalcin@lingoon.com.tr>
2021-08-17 16:28:27 +0300

    LastCode for Api

---
+++ TracingNetCore.Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TracingNetCore.Core.Utilities.Results;
+
+namespace TracingNetCore.Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var result in logics)
+            {
+                if (!result.Success)
+                    return result;
+            }
+            return null;
+        }
+    }
+}
